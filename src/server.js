8791af8584bd8af8e1956d3c917312831b475a64
Kodak, Denys <dkodak@deloitte.ua>
2023-06-26 16:51:11 +0300

    update server

--- src/server.js
+++ src/server.js
@@ -3,11 +3,18 @@ const app = express()
 const path = require('path')
 const fs = require('fs')
 
-app.use('/', express.static(path.resolve(__dirname, '../dist')));
+app.use('/', express.static(path.resolve(__dirname, '../dist')))
+
+app.get('/', (req, res) => {
+    const pathToHTML = path.resolve(__dirname, '../dist/index.html')
+    const contentOfHTML = fs.readFileSync(pathToHTML, 'utf-8')
+    res.send(contentOfHTML)
+})
 
 app.get('*', (req, res) => {
+    console.log('visit route')
     const pathToHTML = path.resolve(__dirname, '../dist/index.html')
-    const contentOfHTML = fs.readFileSync(pathToHTML, 'utf-8');
+    const contentOfHTML = fs.readFileSync(pathToHTML, 'utf-8')
     res.send(contentOfHTML)
 })
 
