b671899b8786be64a48313359e4f7be9f5932f9a
Kodak, Denys <dkodak@deloitte.ua>
2023-07-02 11:17:23 +0300

    update name for html file in server

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const path = require('path')
 app.use(express.static('dist'))
 
 app.get('*', (req, res) => {
-    res.sendFile('index.html', {root: path.join(__dirname, 'dist')});
+    res.sendFile('micro-pets.html', {root: path.join(__dirname, 'dist')});
 })
 
 app.listen(9000, () => {
